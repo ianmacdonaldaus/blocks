53e31ddafdf2e71d7c317f955bbb648d7fcb6e28
Ian MacDonald <ianmacdonaldaus@Ians-MacBook.local>
2012-11-14 08:30:49 +0000

    Initial Commit

---
+++ Blocks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Blocks
+//
+//  Created by Ian MacDonald on 14/11/2012.
+//  Copyright (c) 2012 Ian MacDonald. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Blocks/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Blocks
+//
+//  Created by Ian MacDonald on 14/11/2012.
+//  Copyright (c) 2012 Ian MacDonald. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
